c674111e6f8302ba0a80e055277470ee768d585f
ntirushwjeanmarc <ntirushwajeanmarc@gmail.com>
2024-03-01 15:09:58 +0200

    new change

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+#arduino part
 @app.route('/arduino-gsm')
 def gsm():
     return render_template('arduino/gsm.html')
@@ -11,6 +12,11 @@ def gsm():
 def arduinoSensors():
     return render_template('arduino/sensors.html')
 
+@app.route('/arduino-introduction')
+def arduino_intro():
+    return render_template('arduino/introduction.html')
+#arduino end
+#Python
 @app.route('/python-comments')
 def py_comments():
     return render_template('python/comment.html')
@@ -20,7 +26,7 @@ def py_variables():
 @app.route('/python-data-type')
 def py_data_type():
     return render_template('python/datatype.html')
-
+#End of python
 content_data = [
     {"title": "Introduction to Python", "url": "/python-intro"},
     {"title": "Getting Started with Arduino", "url": "/arduino-start"},
@@ -36,5 +42,6 @@ def search():
     query = request.args.get('q', '').lower()
     results = [content for content in content_data if query in content['title'].lower()]
     return jsonify(results)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
